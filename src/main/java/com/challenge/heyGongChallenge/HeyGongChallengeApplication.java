de40c22834940e32c92d4a5c0a91c9560c907af9
kihangkim <kim.kihang.kiki@gmail.com>
2024-04-08 21:52:40 +0900

    Initial commit

---
+++ src/main/java/com/challenge/heyGongChallenge/HeyGongChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.heyGongChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyGongChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyGongChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/heyGongChallenge/HeyGongChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.heyGongChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyGongChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
